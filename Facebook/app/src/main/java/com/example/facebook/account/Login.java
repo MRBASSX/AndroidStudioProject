f7514c03a2dd3593a6e0fdd602c866c035a5a74a
mrbassx <abassiddrisu9222@gmail.com>
2025-03-16 10:04:32 +0000

    nice

--- Facebook/app/src/main/java/com/example/facebook/account/Login.java
+++ Facebook/app/src/main/java/com/example/facebook/account/Login.java
@@ -3,6 +3,7 @@ package com.example.facebook.account;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,8 +15,10 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.facebook.Home;
 import com.example.facebook.R;
 import com.example.facebook.ui.gallery.GalleryFragment;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
+    TextInputEditText email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +31,48 @@ public class Login extends AppCompatActivity {
             return insets;
         });
 
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
 
     }
 
 
     public void SignIn(View view){
 
-        Intent intent = new Intent(getApplicationContext(), Home.class);
-        startActivity(intent);
+        String UserMail = email.getText().toString().trim();
+        String UserPass = password.getText().toString().trim();
+
+
+        Boolean pass = UserPass.length() > 0;
+        Boolean user = UserMail.length() > 0;
+
+//        Toast.makeText(this, pass+" "+ user, Toast.LENGTH_SHORT).show();
+
+        if (pass.booleanValue() && user.booleanValue()) {
+
+
+            Toast.makeText(this, UserMail +" "+ UserPass, Toast.LENGTH_SHORT).show();
+
+            if (UserPass.equals("Admin") && UserMail.equals("Admin@gmail.com")) {
+
+//                Toast.makeText(this, "Welcome Home", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+
+            }else {
+
+                Toast.makeText(this, "Wrong Credential", Toast.LENGTH_SHORT).show();
+
+            }
+
+
+        }else {
+
+
+            Toast.makeText(this, "All Field Is Required", Toast.LENGTH_SHORT).show();
+        }
+
 
 
 
